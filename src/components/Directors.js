bceccb2de28ade3b5668e898a9be5fd348b37b93
Kevin Ebaugh <kevinebaugh@gmail.com>
2021-11-30 22:14:10 -0500

    commit tree 4a0026393f12fb3e05db9e659c890b5c0269b2ce

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const displayDirectors = directors.map((director) => {
+    return (
+      <div key={director.name}>
+        <h2>Name: {director.name}</h2>
+        <p>Movies:</p>
+        <ul>
+        {director.movies.map((movie, index) => {
+          return <li key={index}>{movie}</li>
+        })}
+        </ul>
+
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {displayDirectors}
+    </div>
+  )
 }
 
 export default Directors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,34 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink
+        to="/"
+        exact
+        style={null}
+        activeStyle={null}
+      > Home </NavLink>
+      <NavLink
+        to="/movies"
+        exact
+        style={null}
+        activeStyle={null}
+      > Movies </NavLink>
+      <NavLink
+        to="/directors"
+        exact
+        style={null}
+        activeStyle={null}
+      > Directors </NavLink>
+      <NavLink
+        to="/actors"
+        exact
+        style={null}
+        activeStyle={null}
+      > Actors </NavLink>
+    </div>
+  )
 }
 
 export default NavBar;
